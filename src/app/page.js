1eb01be65b81acaf6eecae3566bb841e88e07d93
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-05-17 19:05:34 +0600

    add done

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 p-4 text-white">
       <main className="max-w-3xl mx-auto">
-        <h1 className="text-3xl font-bold mb-4 text-center">Task Manager</h1>
+        <h1 className="text-3xl font-bold mb-4 text-center">CareTutors Task Manager</h1>
         {/* <TaskList /> */}
         <TaskForm></TaskForm>
       </main>
